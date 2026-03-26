ac5cfdd27af11f895995c2f3a73fd981fa35dbe8
sinailse <sina.eger@outlook.com>
2026-03-26 07:52:29 +0100

    feat(M2): add PUT /api/keywords/[id]

--- src/app/api/keywords/[id]/route.ts
+++ src/app/api/keywords/[id]/route.ts
@@ -19,3 +19,31 @@ export async function DELETE(
     error,
   });
 }
+
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const supabase = await createClient();
+
+  const { id } = params;
+
+  const body = await request.json();
+
+  const { label, color, description } = body;
+
+  const { data, error } = await supabase
+    .from("keywords")
+    .update({
+      label,
+      color,
+      description,
+    })
+    .eq("id", id)
+    .select();
+
+  return NextResponse.json({
+    data,
+    error,
+  });
+}
